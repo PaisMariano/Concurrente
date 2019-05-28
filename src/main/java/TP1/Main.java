4f55f4e938457767ba15d46e5d0de43616382573
PaisMariano <mariano_a_p@hotmail.com>
2019-05-28 02:07:29 -0300

    agregado el ConcurRadixSort andando falta hacerlo concurrente

--- src/main/java/TP1/Main.java
+++ src/main/java/TP1/Main.java
@@ -1,9 +1,40 @@
 package main.java.TP1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[]args){
-        ThreadPool tPool = new ThreadPool(10, 100);
-        tPool.launch();
+        ConcurRadixSort concur = new ConcurRadixSort();
+        ArrayList<Integer> arraicito = new ArrayList();
+
+        arraicito.add(1);
+        arraicito.add(0);
+        arraicito.add(1);
+        arraicito.add(1);
+        arraicito.add(1);
+        arraicito.add(0);
+        arraicito.add(1);
+        arraicito.add(0);
+        arraicito.add(1);
+        arraicito.add(1);
+        arraicito.add(0);
+        arraicito.add(0);
+        arraicito.add(1);
+        arraicito.add(0);
+        arraicito.add(1);
+        arraicito.add(0);
+        arraicito.add(0);
+        arraicito.add(1);
+        arraicito.add(0);
+        arraicito.add(1);
+
+        System.out.println(arraicito);
+        List temp = concur.radixSort(arraicito);
+        System.out.println(temp);
+
+        //ThreadPool tPool = new ThreadPool(10, 100);
+        //tPool.launch();
     }
 }
